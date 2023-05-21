b9d88b3f2dadd088ead2835f9b109463f0350075
Iliass F <89900801+IliFar@users.noreply.github.com>
2023-05-21 16:20:49 +0200

    Create Navbar with styling

---
+++ Electricity-Prices/Components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5";
+
+const Navbar = ({fontLoaded}) => {
+  return (
+    <View style={styles.navbarcontainer}>
+      <Icon name="bars" size={25} color="white" />
+      {fontLoaded ? (
+        <Text style={styles.text}>Dashboard</Text>
+      ) : (
+        <Text>Loading...</Text>
+      )}
+      <Icon name="bell" size={25} color="white" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  navbarcontainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  text: {
+    color: "white",
+    fontSize: 20,
+    fontFamily: "Roboto-Medium"
+  }
+})
+
+export default Navbar;
